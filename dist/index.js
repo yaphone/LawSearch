cd0bb07f7c5b3beda8d121bd28dc978f2c76b79f
yaphone <zhouyaphone@163.com>
2019-12-24 07:59:26 +0800

    tttt
    tttt

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,16 @@
+// tslint:disable-next-line: no-var-requires
 var fs = require('fs');
+// tslint:disable-next-line: no-var-requires
 var path = require('path');
-var re = new RegExp("\\n第+[\u4e00-\u9fa5]+条+");
+var re = new RegExp('\\n第+[\u4e00-\u9fa5]+条+');
 String.prototype.regexIndexOf = function (regex, startpos) {
     var indexOf = this.substring(startpos || 0).search(regex);
     return (indexOf >= 0) ? (indexOf + (startpos || 0)) : indexOf;
 };
 String.prototype.regexLastIndexOf = function (regex, startpos) {
-    regex = (regex.global) ? regex : new RegExp(regex.source, "g" + (regex.ignoreCase ? "i" : "") + (regex.multiline ? "m" : ""));
-    if (typeof (startpos) == "undefined") {
+    regex = (regex.global) ? regex :
+        new RegExp(regex.source, 'g' + (regex.ignoreCase ? 'i' : '') + (regex.multiline ? 'm' : ''));
+    if (typeof (startpos) === 'undefined') {
         startpos = this.length;
     }
     else if (startpos < 0) {
@@ -17,6 +20,7 @@ String.prototype.regexLastIndexOf = function (regex, startpos) {
     var lastIndexOf = -1;
     var nextStop = 0;
     var result;
+    // tslint:disable-next-line: no-conditional-assignment
     while ((result = regex.exec(stringToWorkWith)) != null) {
         lastIndexOf = result.index;
         regex.lastIndex = ++nextStop;
@@ -24,16 +28,18 @@ String.prototype.regexLastIndexOf = function (regex, startpos) {
     return lastIndexOf;
 };
 function search(keyWord) {
-    var ret = [];
-    var data = fs.readFileSync('./static/law/道路交通事故处理程序规定.txt').toString();
+    // let ret = []
+    var data = fs.readFileSync('./static/law/公安机关办理刑事案件程序规定.txt').toString();
     var pos = data.indexOf(keyWord);
     while (pos > -1) {
         var index1 = data.regexIndexOf(re, pos);
         var index2 = data.regexLastIndexOf(re, pos);
+        // tslint:disable-next-line: no-console
         console.log(data.substring(index2, index1));
+        // tslint:disable-next-line: no-console
         console.log('***********************************');
-        data = data.substring(index2 + 1);
+        data = data.substring(index1 + 1);
         pos = data.indexOf(keyWord);
     }
 }
-search('列情形之一的');
+search('合法');
--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,49 @@
-var fs = require('fs')
-var path = require('path')
+// tslint:disable-next-line: no-var-requires
+const fs = require('fs');
+// tslint:disable-next-line: no-var-requires
+const path = require('path');
 
-var re = new RegExp("\\n第+[\u4e00-\u9fa5]+条+")
+const re = new RegExp('\\n第+[\u4e00-\u9fa5]+条+');
 
 String.prototype.regexIndexOf = function(regex, startpos) {
-  var indexOf = this.substring(startpos || 0).search(regex);
+  const indexOf = this.substring(startpos || 0).search(regex);
   return (indexOf >= 0) ? (indexOf + (startpos || 0)) : indexOf;
-}
+};
 
 String.prototype.regexLastIndexOf = function(regex, startpos) {
-  regex = (regex.global) ? regex : new RegExp(regex.source, "g" + (regex.ignoreCase ? "i" : "") + (regex.multiline ? "m" : ""));
-  if(typeof (startpos) == "undefined") {
+  regex = (regex.global) ? regex :
+   new RegExp(regex.source, 'g' + (regex.ignoreCase ? 'i' : '') + (regex.multiline ? 'm' : ''));
+  if (typeof (startpos) === 'undefined') {
       startpos = this.length;
-  } else if(startpos < 0) {
+  } else if (startpos < 0) {
       startpos = 0;
   }
-  var stringToWorkWith = this.substring(0, startpos + 1);
-  var lastIndexOf = -1;
-  var nextStop = 0;
+  const stringToWorkWith = this.substring(0, startpos + 1);
+  let lastIndexOf = -1;
+  let nextStop = 0;
   let result;
-  while((result = regex.exec(stringToWorkWith)) != null) {
+  // tslint:disable-next-line: no-conditional-assignment
+  while ((result = regex.exec(stringToWorkWith)) != null) {
       lastIndexOf = result.index;
       regex.lastIndex = ++nextStop;
   }
   return lastIndexOf;
-}
-
+};
 
 function search(keyWord: string) {
-  let ret = []
-  let data = fs.readFileSync('./static/law/道路交通事故处理程序规定.txt').toString()
+  // let ret = []
+  let data = fs.readFileSync('./static/law/公安机关办理刑事案件程序规定.txt').toString();
   let pos = data.indexOf(keyWord);
-  while(pos > -1) {
-    let index1 = data.regexIndexOf(re, pos)
-    let index2 = data.regexLastIndexOf(re, pos)
-    console.log(data.substring(index2, index1))
-    console.log('***********************************')
-    data = data.substring(index2 + 1)
-    pos = data.indexOf(keyWord)
+  while (pos > -1) {
+    const index1 = data.regexIndexOf(re, pos);
+    const index2 = data.regexLastIndexOf(re, pos);
+    // tslint:disable-next-line: no-console
+    console.log(data.substring(index2, index1));
+    // tslint:disable-next-line: no-console
+    console.log('***********************************');
+    data = data.substring(index1 + 1);
+    pos = data.indexOf(keyWord);
   }
 }
 
-search('列情形之一的')
+search('合法');
